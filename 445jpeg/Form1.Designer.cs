d8dd7b4db9831629d222b865f24824b5300dc680
Mike W <mwoodmansee@live.com>
2019-12-10 17:58:48 -0500

    Updates to diplaying images , and displays images metadat

--- 445jpeg/Form1.Designer.cs
+++ 445jpeg/Form1.Designer.cs
@@ -29,7 +29,6 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.listBox1 = new System.Windows.Forms.ListBox();
@@ -38,29 +37,21 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(665, 50);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.button1.Location = new System.Drawing.Point(885, 26);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(133, 142);
+            this.button1.Size = new System.Drawing.Size(136, 88);
             this.button1.TabIndex = 0;
             this.button1.Text = "Upload Image";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.Button1_Click);
             // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(805, 50);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(119, 142);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Run Program";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(930, 50);
+            this.button3.Location = new System.Drawing.Point(885, 144);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(118, 142);
+            this.button3.Size = new System.Drawing.Size(136, 93);
             this.button3.TabIndex = 2;
             this.button3.Text = "Exit";
             this.button3.UseVisualStyleBackColor = true;
@@ -68,9 +59,10 @@
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(74, 32);
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pictureBox1.Location = new System.Drawing.Point(1, 0);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(584, 444);
+            this.pictureBox1.Size = new System.Drawing.Size(813, 555);
             this.pictureBox1.TabIndex = 3;
             this.pictureBox1.TabStop = false;
             // 
@@ -78,9 +70,9 @@
             // 
             this.listBox1.FormattingEnabled = true;
             this.listBox1.ItemHeight = 16;
-            this.listBox1.Location = new System.Drawing.Point(665, 222);
+            this.listBox1.Location = new System.Drawing.Point(832, 266);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(383, 228);
+            this.listBox1.Size = new System.Drawing.Size(223, 276);
             this.listBox1.TabIndex = 4;
             // 
             // Form1
@@ -91,9 +83,8 @@
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
@@ -104,7 +95,6 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.ListBox listBox1;
--- 445jpeg/Form1.cs
+++ 445jpeg/Form1.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,19 +24,74 @@ namespace _445jpeg
 
             OpenFileDialog of = new OpenFileDialog();
             //For any other formats
-            of.Filter = "Image Files (*.bmp;*.jpg;*.jpeg,*.png)|*.BMP;*.JPG;*.JPEG;*.PNG";
+            of.Filter = "Image Files (*.jpg;*.jpeg;)|*.JPG;*.JPEG;";
             if (of.ShowDialog() == DialogResult.OK)
             {
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
                 pictureBox1.ImageLocation = of.FileName;
 
+                //code below is from https://www.dreamincode.net/forums/topic/231165-how-to-read-image-metadata-in-c%23/
+                // i changed the code to display in listbox1
+                
 
+                Bitmap image = new Bitmap(of.FileName);
+                PropertyItem[] propItems = image.PropertyItems;
+                int count = 0;
+                foreach (PropertyItem item in propItems)
+                {
+                    listBox1.Items.Add("Property Item " + count.ToString());
+                    listBox1.Items.Add("iD: 0x" + item.Id.ToString("x"));
+                    count++;
+                }
+
+                ASCIIEncoding encodings = new ASCIIEncoding();
+                try
+                {
+                    string make = encodings.GetString(propItems[1].Value);
+                    listBox1.Items.Add("The equipment make is " + make + ".");
+                }
+                catch
+                {
+                    listBox1.Items.Add("no Meta Data Found");
+                }
+                try
+                {
+                    string model = encodings.GetString(propItems[2].Value);
+                    listBox1.Items.Add("The model is " + model + ".");
+                }
+                catch
+                {
+                    listBox1.Items.Add("no Model Found");
+                }
+                try
+                {
+                    string DT = encodings.GetString(propItems[15].Value);
+                    listBox1.Items.Add("The Date & Time is " + DT + ".");
+                }
+                catch
+                {
+                    listBox1.Items.Add("no date Found");
+                }
+                // end of reused code
             }
+
+           
+
+
+
+
+
+            
+
         }
+       
+    
 
         private void Button3_Click(object sender, EventArgs e)
         {
             this.Close();
-
         }
+
     }
+
 }
